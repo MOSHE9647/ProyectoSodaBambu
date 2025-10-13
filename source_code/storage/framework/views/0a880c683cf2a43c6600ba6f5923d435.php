de2160422f1e2608eca47e0c22fc7212cabcbba7
Moshe9647 <isaacmhp2001@gmail.com>
2025-10-13 00:23:27 -0600

    feat: Enhance offline page with theme support, loading state, and improved UI elements. Also added .gitignore to exclude all files except itself in storage/frameworks/views

--- source_code/storage/framework/views/0a880c683cf2a43c6600ba6f5923d435.php
+++
@@ -1,4 +0,0 @@
-<svg fill="none" stroke="currentColor" viewBox="0 0 24 24" <?php echo e($attributes); ?>>
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
-</svg>
-<?php /**PATH C:\Users\isaac\OneDrive - Universidad Nacional de Costa Rica\2025\II CICLO 2025\INGENIERIA EN SISTEMAS II\Proyecto\ProyectoSodaBambu\source_code\vendor\laravel\framework\src\Illuminate\Foundation\Providers/../resources/exceptions/renderer/components/icons/check.blade.php ENDPATH**/ ?>
