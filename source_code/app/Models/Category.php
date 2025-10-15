6d1a038027cc094511cd2775673854ea357ca732
MelanieOviedo <oviedomelanie07@gmail.com>
2025-10-15 17:05:00 -0600

    feat: Backend of the category CRUD completed, the category views must be recreated since they were only for testing

---
+++ source_code/app/Models/Category.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use App\Casts\CostaRicaDatetime;
+use Database\Factories\CategoryFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    /** @use HasFactory<CategoryFactory> */
+	use HasFactory, SoftDeletes;
+
+    /**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var list<string>
+	 */
+	protected $fillable = [
+		'name',
+		'description',
+	];
+
+
+    /**
+	 * Get the attributes that should be cast.
+	 *
+	 * @return array<string, string>
+	 */
+	protected $casts = [
+		'created_at' => CostaRicaDatetime::class,
+		'updated_at' => CostaRicaDatetime::class,
+		'deleted_at' => CostaRicaDatetime::class,
+	];
+
+}
