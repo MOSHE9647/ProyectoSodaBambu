72e2bf692fd571eb03c98b8bd4f73d2aa4af93bd
Jeremy Romero Carazo <117392636+Romero42@users.noreply.github.com>
2025-10-16 17:50:18 -0600

    Added controller and route with temporary JSON responses for testing.

--- source_code/routes/web.php
+++ source_code/routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MethodPaymentController;
 /**
  * Evaluate the user's role and redirect accordingly.
  * If the user is not authenticated, redirect to the login page.
@@ -17,3 +17,9 @@ Route::middleware(['auth', 'verified', 'prevent-back'])->group(function () {
 	Route::get('/dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
 	Route::get('/sales', [HomeController::class, 'sales'])->name('sales');
 });
+
+/**
+ * Payment Method Routes made with Resource Controller
+ * and JSON responses for testing.
+ */
+Route::resource('payment', MethodPaymentController::class);
