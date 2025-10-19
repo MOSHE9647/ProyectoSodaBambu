05e269b02c2bc862b74309054380ac4967c5108e
Nataliaortiz9 <deyaneira.ortiz.martinez@est.una.ac.cr>
2025-10-18 19:46:29 -0600

    The supplier path is added to web.php
    The supplier route is added

--- source_code/routes/web.php
+++ source_code/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SupplierController;
 use Illuminate\Support\Facades\Route;
 
 /**
@@ -16,4 +17,7 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::middleware(['auth', 'verified', 'prevent-back'])->group(function () {
 	Route::get('/dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
 	Route::get('/sales', [HomeController::class, 'sales'])->name('sales');
+	
+	// Supplier routes
+	Route::resource('suppliers', SupplierController::class);
 });
