c0b222aee7e97e8fa76f693069cc5abcb13cf066
Nataliaortiz9 <deyaneira.ortiz.martinez@est.una.ac.cr>
2025-10-18 19:58:21 -0600

    create suppliers table with basic fields
    add suppliers table migration (name, phone, email, timestamps)

--- source_code/database/migrations/2025_10_18_221801_suppliers_table.php
+++ source_code/database/migrations/2025_10_18_221801_suppliers_table.php
@@ -11,7 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id('id_supplier');
+            $table->string('name');
+            $table->string('phone');
+            $table->string('email');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('suppliers');
     }
 };
