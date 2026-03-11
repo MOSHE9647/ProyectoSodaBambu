387994fb86ce40e79092143aa1ee1876734bfc85
Natalia Ortiz Martinez <deyaneira.ortiz.martinez@est.una.ac.cr>
2026-03-10 23:04:06 -0600

    Create edit.blade.php

---
+++ source_code/resources/views/models/products/edit.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('models.products.form', [
+        'action' => route('products.update', $product->id),
+        'product' => $product
+    ])
+@endsection
