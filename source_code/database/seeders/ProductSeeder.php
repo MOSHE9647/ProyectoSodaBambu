4184e61e4a4a93a9cbc1ca0f48219fab573a6ecc
Natalia Ortiz Martinez <deyaneira.ortiz.martinez@est.una.ac.cr>
2026-03-11 22:26:47 -0600

    add description of barcode

--- source_code/database/seeders/ProductSeeder.php
+++ source_code/database/seeders/ProductSeeder.php
@@ -24,16 +24,16 @@ class ProductSeeder extends Seeder
         $catBebida = Category::where('name', 'Licores')->first()->id;
 
         $products = [
-            ['category_id' => $catDesayuno, 'name' => 'Gallo Pinto Especial', 'type' => ProductType::DISH, 'has_inventory' => true, 'reference_cost' => 1200, 'tax_percentage' => 13, 'margin_percentage' => 150, 'sale_price' => 3000],
-            ['category_id' => $catFuerte, 'name' => 'Casado con Carne en Salsa', 'type' => ProductType::DISH, 'has_inventory' => true, 'reference_cost' => 1800, 'tax_percentage' => 13, 'margin_percentage' => 122, 'sale_price' => 4000],
-            ['category_id' => $catFuerte, 'name' => 'Arroz con Pollo', 'type' => ProductType::DISH, 'has_inventory' => true, 'reference_cost' => 1500, 'tax_percentage' => 13, 'margin_percentage' => 133, 'sale_price' => 3500],
-            ['category_id' => $catFuerte, 'name' => 'Chifrijo Grande', 'type' => ProductType::DISH, 'has_inventory' => true, 'reference_cost' => 2000, 'tax_percentage' => 13, 'margin_percentage' => 125, 'sale_price' => 4500],
-            ['category_id' => $catBebida, 'name' => 'Cerveza Imperial (Botella)', 'type' => ProductType::DRINK, 'has_inventory' => true, 'reference_cost' => 950, 'tax_percentage' => 13, 'margin_percentage' => 57, 'sale_price' => 1500],
-            ['category_id' => $catBebida, 'name' => 'Cerveza Pilsen (Botella)', 'type' => ProductType::DRINK, 'has_inventory' => true, 'reference_cost' => 950, 'tax_percentage' => 13, 'margin_percentage' => 57, 'sale_price' => 1500],
-            ['category_id' => $catFuerte, 'name' => 'Olla de Carne (Fin de semana)', 'type' => ProductType::DISH, 'has_inventory' => false, 'reference_cost' => 2200, 'tax_percentage' => 13, 'margin_percentage' => 104, 'sale_price' => 4500],
-            ['category_id' => $catFuerte, 'name' => 'Hamburguesa Artesanal', 'type' => ProductType::DISH, 'has_inventory' => true, 'reference_cost' => 2500, 'tax_percentage' => 13, 'margin_percentage' => 100, 'sale_price' => 5000],
-            ['category_id' => $catDesayuno, 'name' => 'Omelette con Tostadas', 'type' => ProductType::DISH, 'has_inventory' => true, 'reference_cost' => 1100, 'tax_percentage' => 13, 'margin_percentage' => 154, 'sale_price' => 2800],
-            ['category_id' => $catFuerte, 'name' => 'Filete de Pescado al Ajillo', 'type' => ProductType::DISH, 'has_inventory' => true, 'reference_cost' => 2800, 'tax_percentage' => 13, 'margin_percentage' => 96, 'sale_price' => 5500],
+            ['category_id' => $catDesayuno, 'barcode' => '2000000000001', 'name' => 'Gallo Pinto Especial', 'type' => ProductType::DISH, 'has_inventory' => true, 'reference_cost' => 1200, 'tax_percentage' => 13, 'margin_percentage' => 150, 'sale_price' => 3000],
+            ['category_id' => $catFuerte, 'barcode' => '2000000000002', 'name' => 'Casado con Carne en Salsa', 'type' => ProductType::DISH, 'has_inventory' => true, 'reference_cost' => 1800, 'tax_percentage' => 13, 'margin_percentage' => 122, 'sale_price' => 4000],
+            ['category_id' => $catFuerte, 'barcode' => '2000000000003', 'name' => 'Arroz con Pollo', 'type' => ProductType::DISH, 'has_inventory' => true, 'reference_cost' => 1500, 'tax_percentage' => 13, 'margin_percentage' => 133, 'sale_price' => 3500],
+            ['category_id' => $catFuerte, 'barcode' => '2000000000004', 'name' => 'Chifrijo Grande', 'type' => ProductType::DISH, 'has_inventory' => true, 'reference_cost' => 2000, 'tax_percentage' => 13, 'margin_percentage' => 125, 'sale_price' => 4500],
+            ['category_id' => $catBebida, 'barcode' => '2000000000005', 'name' => 'Cerveza Imperial (Botella)', 'type' => ProductType::DRINK, 'has_inventory' => true, 'reference_cost' => 950, 'tax_percentage' => 13, 'margin_percentage' => 57, 'sale_price' => 1500],
+            ['category_id' => $catBebida, 'barcode' => '2000000000006', 'name' => 'Cerveza Pilsen (Botella)', 'type' => ProductType::DRINK, 'has_inventory' => true, 'reference_cost' => 950, 'tax_percentage' => 13, 'margin_percentage' => 57, 'sale_price' => 1500],
+            ['category_id' => $catFuerte, 'barcode' => '2000000000007', 'name' => 'Olla de Carne (Fin de semana)', 'type' => ProductType::DISH, 'has_inventory' => false, 'reference_cost' => 2200, 'tax_percentage' => 13, 'margin_percentage' => 104, 'sale_price' => 4500],
+            ['category_id' => $catFuerte, 'barcode' => '2000000000008', 'name' => 'Hamburguesa Artesanal', 'type' => ProductType::DISH, 'has_inventory' => true, 'reference_cost' => 2500, 'tax_percentage' => 13, 'margin_percentage' => 100, 'sale_price' => 5000],
+            ['category_id' => $catDesayuno, 'barcode' => '2000000000009', 'name' => 'Omelette con Tostadas', 'type' => ProductType::DISH, 'has_inventory' => true, 'reference_cost' => 1100, 'tax_percentage' => 13, 'margin_percentage' => 154, 'sale_price' => 2800],
+            ['category_id' => $catFuerte, 'barcode' => '2000000000010', 'name' => 'Filete de Pescado al Ajillo', 'type' => ProductType::DISH, 'has_inventory' => true, 'reference_cost' => 2800, 'tax_percentage' => 13, 'margin_percentage' => 96, 'sale_price' => 5500],
         ];
 
         foreach ($products as $product) {
