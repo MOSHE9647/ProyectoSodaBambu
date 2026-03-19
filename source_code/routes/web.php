dce4f7f4b022eb9429229ea97dbd2e21e7e4102c
Melanie Oviedo Maleaño <oviedomelanie07@gmail.com>
2026-03-18 19:59:05 -0600

    Cleaning up web.php removing unnecessary imports and comenta

--- source_code/routes/web.php
+++ source_code/routes/web.php
@@ -12,9 +12,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
 
 // imports for testing routes
-use App\Http\Controllers\TestStockController;
-use App\Models\Product;
-use Illuminate\Http\Request;
+use App\Http\Controllers\TestStockController;;
 
 /**
  * Evaluate the user's role and redirect accordingly.
@@ -50,57 +48,5 @@ Route::middleware(['auth', 'verified', 'prevent-back'])->group(function () {
 	// Route for testing low stock warning
 	 Route::get('/test-low-stock/{stock}', [TestStockController::class, 'triggerLowStock'])->name('test.low-stock');
 
-	// //Route for testing low stock and expiring soon filters
-	// Route::get('/products', function (Request $request) {
-	// 	// If the 'filter' parameter is 'low_stock', we apply the database logic
-	// 	if ($request->query('filter') === 'low_stock') {
-	// 		$products = Product::with('stock')
-	// 			->whereHas('stock', function($query) {
-	// 				// We compare the columns of the product_stocks table
-	// 				$query->whereColumn('current_stock', '<=', 'minimum_stock');
-	// 			})
-	// 			->get();
-				
-	// 		return response()->json([
-	// 			'status' => 'Filtrado aplicado: Stock Bajo',
-	// 			'count' => $products->count(),
-	// 			'results' => $products
-	// 		]);
-	// 	}
 
-	// 	// If no filter is applied, we display all (or what you prefer for testing)
-	// 	return response()->json([
-	// 		'status' => 'Mostrando todos los productos',
-	// 		'results' => Product::with('stock')->get()
-	// 	]);
-	// })->name('products.index');
-
-	// Route::get('/supplies', function (Request $request) {
-	// 	if ($request->query('filter') === 'expiring_soon') {
-	// 		// We define "expiring soon" as the next 7 days
-	// 		$limitDate = now()->addDays(7);
-	// 		// We fetch supplies that have purchase details with expiration dates within the next 7 days
-	// 		$supplies = Supply::with(['purchaseDetails' => function($query) use ($limitDate) {
-	// 				$query->where('expiration_date', '>', now())
-	// 					->where('expiration_date', '<=', $limitDate);
-	// 			}])
-	// 			// We ensure we only get supplies that have at least one purchase detail expiring soon
-	// 			->whereHas('purchaseDetails', function($query) use ($limitDate) {
-	// 				$query->where('expiration_date', '>', now())
-	// 					->where('expiration_date', '<=', $limitDate);
-	// 			})
-	// 			->get();
-
-	// 		return response()->json([
-	// 			'status' => 'Filtrado: Insumos próximos a vencer (7 días)',
-	// 			'count' => $supplies->count(),
-	// 			'results' => $supplies
-	// 		]);
-	// 	}
-
-	// 	return response()->json([
-	// 		'status' => 'Todos los insumos',
-	// 		'results' => Supply::all()
-	// 	]);
-	// })->name('supplies.index');
 });
