9b2d7a688c96645b6e39c3fa7b96a6995cbcce56
MOSHE9647 <62158167+MOSHE9647@users.noreply.github.com>
2026-05-04 17:16:44 +0000

    Fixed code style with Laravel Pint

--- source_code/app/Models/Contract.php
+++ source_code/app/Models/Contract.php
@@ -75,6 +75,7 @@ class Contract extends Model
         if ($this->start_date && $this->end_date) {
             $start = $this->start_date->locale('es')->isoFormat('D MMM YYYY');
             $end = $this->end_date->locale('es')->isoFormat('D MMM YYYY');
+
             return "$start - $end";
         }
 
