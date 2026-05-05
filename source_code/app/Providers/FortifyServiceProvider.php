cd85c0d07185283aae21a11be392efad80b45c60
Natalia Ortiz Martinez <deyaneira.ortiz.martinez@est.una.ac.cr>
2026-05-05 11:38:22 -0600

    General Tests
    Unit, system, and comprehensive tests for monitoring number seven

--- source_code/app/Providers/FortifyServiceProvider.php
+++ source_code/app/Providers/FortifyServiceProvider.php
@@ -70,8 +70,11 @@ class FortifyServiceProvider extends ServiceProvider
 
         // Configure rate limiting for login and two-factor authentication attempts.
         RateLimiter::for('login', function (Request $request) {
-            $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())).'|'.$request->ip());
+            if (app()->environment('local', 'testing')) {
+                return Limit::none();
+            }
 
+            $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())).'|'.$request->ip());
             return Limit::perMinute(5)->by($throttleKey);
         });
 
