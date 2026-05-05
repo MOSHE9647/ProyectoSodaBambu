4c1084ef217f643cf09590a91cda01652058251f
DeyaneiraOrtizMartinez <236527442+DeyaneiraOrtizMartinez@users.noreply.github.com>
2026-05-05 17:39:21 +0000

    Fixed code style with Laravel Pint

--- source_code/tests/Pest.php
+++ source_code/tests/Pest.php
@@ -3,6 +3,7 @@
 use App\Enums\UserRole;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Pest\Browser\Api\Webpage;
 use Tests\TestCase;
 
 /*
@@ -58,9 +59,10 @@ function actingAsEmployee(): void
 
 /**
  * Helper function to log in as a specific user in browser tests.
- * @param mixed $user The user instance to log in as.
- * @param mixed $password The password for the user.
- * @return Pest\Browser\Api\Webpage The webpage instance after logging in, allowing for further interactions in the test.
+ *
+ * @param  mixed  $user  The user instance to log in as.
+ * @param  mixed  $password  The password for the user.
+ * @return Webpage The webpage instance after logging in, allowing for further interactions in the test.
  */
 function loginAsUser($user, $password)
 {
