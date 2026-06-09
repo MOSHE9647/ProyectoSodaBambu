06dabc8f302855b601e4a0beb9d56a6e3bc0f3e7
Moshe9647 <isaac.herrera.pastrana@est.una.ac.cr>
2026-06-08 18:14:26 -0600

    fix: Fix Newman (Postman) CLI to run API tests inside the GitHub Actions Tests workflow

--- source_code/app/Providers/AppServiceProvider.php
+++ source_code/app/Providers/AppServiceProvider.php
@@ -51,7 +51,7 @@ class AppServiceProvider extends ServiceProvider
         Transaction::observe(TransactionObserver::class);
 
         // Force HTTPS in production
-        if (config('app.env') !== 'local') {
+        if (config('app.env') !== 'local' && config('app.env') !== 'testing') {
             URL::forceScheme('https');
         }
 
